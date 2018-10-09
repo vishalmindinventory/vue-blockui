5c113cee9ef85497b1050b15b7c985b74d18bbad
vishal-php <vishal.mindinventory@gmail.com>
2018-10-09 18:13:21 +0530

    - Update style

--- src/components/BlockUI.vue
+++ src/components/BlockUI.vue
@@ -48,6 +48,10 @@ export default {
       border-radius: 0;
       left: 50%;
       top: 50%;
+      -ms-transform: translate(-50%,-50%);
+      -moz-transform: translate(-50%,-50%);
+      -webkit-transform: translate(-50%,-50%);
+      transform: translate(-50%,-50%);
       .loading-icon {
             margin-bottom: 8px;
       }
